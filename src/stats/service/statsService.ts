36ba64f76b3370584c244a0bec5187903f03f232
Guillem Blasco <gowet92@gmail.com>
2024-07-04 18:43:02 +0200

    Add function getCourseStats that allows us see all course stadistics

--- src/stats/service/statsService.ts
+++ src/stats/service/statsService.ts
@@ -1,7 +1,60 @@
-import { courses, grades } from "../../index.js";
+import { getGradesTotal } from "../../grades/service/gradesService.js";
+import { courses, grades, students } from "../../index.js";
+import { getStudentsTotal } from "../../students/service/studentsService.js";
 import { CourseStats } from "../../types";
 
-// Crea una función para obtener las estadísticas de un curso
-// La función debe recibir el id de un curso
-// La función debe devolver un objeto de tipo CourseStats
-// export const getCourseStats =
+export const getCourseStats = (courseId: number): CourseStats => {
+  const courseStatsId = grades.filter((grade) => grade.courseId === courseId);
+
+  const approvedCriteria: number = 5;
+
+  let failedCount: number = 0;
+
+  const studentsCount = courseStatsId.length;
+
+  for (const grade of courseStatsId) {
+    if (grade.value < approvedCriteria) failedCount++;
+  }
+
+  const passedCount = courseStatsId.length - failedCount;
+
+  const failedCountPercentage: number =
+    (failedCount / courseStatsId.length) * 100;
+
+  const passedPercentGrades: number = 100 - failedCountPercentage;
+
+  let averageGrade: number = 0;
+
+  for (let grade = 0; grade < courseStatsId.length; grade++) {
+    if (grade != courseStatsId.length - 1) {
+      averageGrade += courseStatsId[grade].value;
+    } else {
+      averageGrade += courseStatsId[grade].value;
+      averageGrade = averageGrade / courseStatsId.length;
+    }
+  }
+
+  let highestGrade: number = 0;
+
+  let highestGradeStudentId: number = 0;
+
+  for (const grade of courseStatsId) {
+    if (grade.value >= highestGrade) {
+      highestGrade = grade.value;
+      highestGradeStudentId = grade.studentId;
+    }
+  }
+
+  const courseStats: CourseStats = {
+    courseId: courseId,
+    studentsCount: studentsCount,
+    passedCount: passedCount,
+    passedCountPercentage: passedPercentGrades,
+    failedCount: failedCount,
+    failedCountPercentage: failedCountPercentage,
+    averageGrade: averageGrade,
+    highestGrade: highestGrade,
+    highestGradeStudentId: highestGradeStudentId,
+  };
+  return courseStats;
+};
